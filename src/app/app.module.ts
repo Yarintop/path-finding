ffdcd16dd0b282e6f6e972c63a39d02f6de2923c
Yarintop <yarintop2@gmail.com>
2020-11-05 02:46:25 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MazeComponent } from './maze/maze.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MazeComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule
